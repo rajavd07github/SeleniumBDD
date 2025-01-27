15862da7f80f797d3841fafca4284714b2a0cbd2
rajavd07github <165901960+rajavd07github@users.noreply.github.com>
2025-01-27 13:13:57 +0530

    last commit

--- features/steps/step_def.py
+++ features/steps/step_def.py
@@ -2,24 +2,48 @@ import time
 
 from behave import given, when, then
 from selenium import webdriver
-from selenium.webdriver.common import by
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @given('User is on the login page')
 def step_user_on_login(context):
+    print("@given stated")
     context.browser = webdriver.Chrome()
+    context.browser.maximize_window()
     context.browser.get("https://www2.hm.com/en_in/index.html")
+    # wait for the Accept all cookies to be clickable
+    WebDriverWait(context.browser,5).until(
+        EC.element_to_be_clickable((By.ID,"onetrust-accept-btn-handler"))).click()
+    #To wait until URL is changes
+    WebDriverWait(context.browser,10).until(
+        EC.url_contains("https://www2.hm.com/en_in/index.html"))
     assert "https://www2.hm.com/en_in/index.html" in context.browser.current_url
-    time.sleep(5)
+
 
 
 @when('User enters valid credentials')
 def steps_to_enter_credentials(context):
-    context.browser.find_element_by_cssselector("#email").sendkeys("hi@gmail.com")
-    context.browser.find_element_by_name("Login/ Sign Up").sendkeys("raja.viknesh@gmail.com")
+    print("when stated")
+    # click on signin button
+    context.browser.find_element(By.CSS_SELECTOR,".CGae.mYRh.vEfo.__5DXf.k0Zy").click()
+    time.sleep(2)
+    # Enter the username
+    context.browser.find_element(By.CSS_SELECTOR,"#email").send_keys("raja.viknesh@outlook.com")
+    # Enter the Password
+    context.browser.find_element(By.CSS_SELECTOR,"#password").send_keys("Loveis@07")
+    # Click on submit button
+    context.browser.find_element(By.CSS_SELECTOR,"button[type='submit']").click()
+    print("when Ended")
+
 
 @then('User is redirected to the homepage')
 def steps_to_redirect(context):
-    assert "https://www.redbus.in/" in context.currenturl
+    WebDriverWait(context.browser,5).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR,".SignInBox-module--signInText__1e_FN"))).click()
+    WebDriverWait(context.browser,10).until(
+        EC.url_contains("https://www2.hm.com/en_in/account"))
+    assert "https://www2.hm.com/en_in/account" in context.browser.current_url
     context.browser.quit()
 
